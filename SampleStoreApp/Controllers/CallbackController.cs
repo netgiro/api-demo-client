1c80c6d53efc127212af922eb8f16177dfb6e354
Mario Balić <mario.balic1@gmail.com>
2019-11-22 14:25:14 +0100

    signature check on callback

--- SampleStoreApp/Controllers/CallbackController.cs
+++ SampleStoreApp/Controllers/CallbackController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using NetgiroClient;
+using NetgiroClient.Helpers;
 using NetgiroClient.Models;
 using SampleStoreApp.SignalR;
-using System.Threading.Tasks;
 
 namespace SampleStoreApp.Controllers
 {
@@ -20,7 +22,18 @@ namespace SampleStoreApp.Controllers
         [HttpPost]
         public async Task<ActionResult> Callback([FromBody] PaymentResponse paymentResponse)
         {
-            await this._hubcontext.Clients.Client(_clientManager.GetClientByTransactionId(paymentResponse.PaymentInfo.TransactionId)).SendAsync("ReceiveMessage", "user", "Callback received");
+            var appKey = this.Request.Headers[Constants.Netgiro_AppKey];
+            var signature = this.Request.Headers[Constants.Netgiro_Signature];
+            var nonce = this.Request.Headers[Constants.Netgiro_Nonce];
+            var transactionId = paymentResponse.PaymentInfo.TransactionId;
+            var calculatedSignature = Signature.CalculateSignature(appKey, transactionId, nonce);
+
+            if (signature != calculatedSignature)
+            {
+                return Unauthorized("Invalid signature");
+            }
+
+            await this._hubcontext.Clients.Client(_clientManager.GetClientByTransactionId(transactionId)).SendAsync("ReceiveMessage", "user", "Callback received");
 
             return Ok();
         }
